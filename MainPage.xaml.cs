fec9f6c765018aef81fcfa97cd03eb1fc3385dd2
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2020-04-19 12:10:31 -0400

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Print_PDF_Demo
+{
+    /// <summary>
+    /// Lógica de interacción para MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.IsEnabled = false;
+                PrintDialog My_printDialog = new PrintDialog();
+                if(My_printDialog.ShowDialog() == true)
+                {
+                    My_printDialog.PrintVisual(forPrint, "My PDF print");
+                }
+            }
+            finally
+            {
+                this.IsEnabled = true;
+            }
+        }
+    }
+}
